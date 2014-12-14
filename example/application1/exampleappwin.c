18921531802c38259112bda266a983b01c27a9eb
MingpeiZheng <yueyawanbian@gmail.com>
2014-12-14 13:05:09 +0800

    根据master修改gh-pages

---
+++ example/application1/exampleappwin.c
@@ -0,0 +1,38 @@
+#include <gtk/gtk.h>
+
+#include "exampleapp.h"
+#include "exampleappwin.h"
+
+struct _ExampleAppWindow
+{
+  GtkApplicationWindow parent;
+};
+
+struct _ExampleAppWindowClass
+{
+  GtkApplicationWindowClass parent_class;
+};
+
+G_DEFINE_TYPE(ExampleAppWindow, example_app_window, GTK_TYPE_APPLICATION_WINDOW);
+
+static void
+example_app_window_init (ExampleAppWindow *app)
+{
+}
+
+static void
+example_app_window_class_init (ExampleAppWindowClass *class)
+{
+}
+
+ExampleAppWindow *
+example_app_window_new (ExampleApp *app)
+{
+  return g_object_new (EXAMPLE_APP_WINDOW_TYPE, "application", app, NULL);
+}
+
+void
+example_app_window_open (ExampleAppWindow *win,
+                         GFile            *file)
+{
+}
---
+++ example/application1/index.md
@@ -0,0 +1,15 @@
+##官方范例1
+
+###源文件目录
+
+* application1.md
+* exampleapp.c
+* exampleapp.desktop
+* exampleapp.h
+* exampleapp.png
+* exampleappwin.c
+* exampleappwin.h
+* main.c
+* Makefile.am
+* Makefile.example
+* README
